8f396c82025d537f13be4231e541292a1172e88d
Karen Da Cruz <me@karendacruz.com>
2013-03-27 20:13:09 -0500

    deploying

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
@@ -58,5 +59,9 @@ module Railsgirls
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    #heroku fails
+    config.assets.initialize_on_precompile = false
+    
   end
 end
