448c08db9d3416d77858f7357f3e88e3342c5442
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2013-04-15 20:37:14 -0500

    kept secret token private

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,7 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Railsgirls::Application.config.secret_token = 'e22aca40afedd5ecc9c67bdbef088c3e7c2d42541b52070dee45ca239e018d8aa7a08d1fb083a0d87599d01e7b8d0606ac13dc58368119f13f37872c9bf8978b'
+
+# Make sure your secret_token is kept private
+# if you're sharing your code publicly.
+Railsgirls::Application.config.secret_token = Rails.env.production?? ENV['SECRET_TOKEN'] : 'e22aca40afedd5ecc9c67bdbef088c3e7c2d42541b52070dee45ca239e018d8aa7a08d1fb083a0d87599d01e7b8d0606ac13dc58368119f13f37872c9bf8978b'
