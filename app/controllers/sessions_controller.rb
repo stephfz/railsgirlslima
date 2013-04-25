7cfd9d9682cb15557c1e60a9c1f1a53d2098fa12
Stephanie Frias <stephfz@gmail.com>
2013-04-25 18:17:47 -0500

    FB integration

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,18 +3,10 @@ class SessionsController < ApplicationController
   end
 
 def create
-  auth_hash = request.env['omniauth.auth']
- 
-  @authorization = Authorization.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
-  if @authorization
-    render :text => "Welcome back #{@authorization.user.name}! You have already signed up."
-  else
-    user = User.new :name => auth_hash["info"]["name"], :email => auth_hash["info"]["email"]
-    user.authorizations.build :provider => auth_hash["provider"], :uid => auth_hash["uid"]
-    user.save
- 
-    render :text => "Hi #{user.name}! You've signed up."
-  end
+    auth = request.env["omniauth.auth"]
+    user = Authorization.find_by_provider_and_uid(auth["provider"], auth["uid"]) || Authorization.create_with_omniauth(auth)
+    session[:user_id] = user.id
+    redirect_to root_url, :notice => "Signed in!"
 end
 
   def failure
